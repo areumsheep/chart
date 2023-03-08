022218d65dc6389178dadd5308137b852ebe227c
areumsheep <areumsheep@gmail.com>
2023-03-08 18:39:47 +0900

    Add: 첫 번째 라인 차트 추가

--- src/LineChart.ts
+++ src/LineChart.ts
@@ -2,8 +2,10 @@ import type { Data, Datum } from './types/Data';
 import { padLeft } from './utils/string';
 
 const DEFAULT_AXIS_PADDING = 20;
-const DURATION = 1000 * 30;
+const DURATION = 1000 * 60;
 const MAX_Y = 100;
+
+const xTick = 5;
 
 class LineChart {
   $canvas: HTMLCanvasElement;
@@ -12,7 +14,8 @@ class LineChart {
   chartHeight: number;
   startTime!: number;
   endTime!: number;
-  xTimeInterval = 5000;
+  xTimeInterval = DURATION;
+  data: Data = [];
 
   constructor($canvas: HTMLCanvasElement) {
     this.$canvas = $canvas;
@@ -25,7 +28,7 @@ class LineChart {
 
   #setTime = () => {
     this.endTime = Date.now();
-    this.startTime = this.endTime - DURATION;
+    this.startTime = this.endTime - DURATION * xTick;
   };
 
   #drawAxisX() {
@@ -43,36 +46,26 @@ class LineChart {
   }
 
   #drawLabelX() {
-    // X 축 눈금 간격
-    const xTickInterval = 60;
-
-    // X 축 최대 출력 수
-    const xTick = 5;
-
-    const date = new Date();
-    date.setMinutes(date.getMinutes() - (xTick - 1));
-    date.setSeconds(0);
-    date.setMilliseconds(0);
-
-    const fiveMinutesAgo = date.getTime();
+    const { endTime, startTime, xTimeInterval, chartWidth, chartHeight } = this;
 
     // X 축 눈금 그리기
     this.ctx.beginPath();
-    for (let i = 0; i <= xTick; i++) {
-      const x =
-        i * xTickInterval -
-        (((Date.now() - fiveMinutesAgo) / 1000) % xTickInterval);
 
-      const time = new Date(fiveMinutesAgo + x * 1000);
-      const hour = padLeft(`${time.getHours()}`, '0', 2);
-      const minute = padLeft(`${time.getMinutes()}`, '0', 2);
+    let currentTime = startTime - (startTime % xTimeInterval);
+    this.ctx.textAlign = 'center';
+    this.ctx.textBaseline = 'top';
+    while (currentTime < endTime + DURATION * 5) {
+      const xPoint = ((currentTime - startTime) / DURATION) * chartWidth * 0.2;
 
+      const date = new Date(currentTime);
+      const hour = padLeft(`${date.getHours()}`, '0', 2);
+      const minute = padLeft(`${date.getMinutes()}`, '0', 2);
       const text = `${hour}:${minute}`;
 
-      this.ctx.textAlign = 'center';
-      this.ctx.fillText(text, x * 1.5, this.chartHeight + 20);
-      this.ctx.moveTo(x * 1.5, this.chartHeight);
-      this.ctx.lineTo(x * 1.5, this.chartHeight + 10);
+      this.ctx.fillText(text, xPoint, chartHeight + 8);
+      this.ctx.moveTo(xPoint, this.chartHeight);
+      this.ctx.lineTo(xPoint, this.chartHeight + 7);
+      currentTime += xTimeInterval;
     }
     this.ctx.stroke();
   }
@@ -85,9 +78,10 @@ class LineChart {
     this.ctx.textAlign = 'right';
     for (let i = 0; i <= yTickInterval; i++) {
       const value = i * yTickInterval;
+
       const yPoint =
         this.chartHeight - (value / MAX_Y) * this.chartHeight * 0.9;
-      this.ctx.fillText(`${value}`, DEFAULT_AXIS_PADDING - 4, yPoint + 3);
+      this.ctx.fillText(`${value}`, DEFAULT_AXIS_PADDING - 4, yPoint - 5);
       if (i !== 0) {
         this.ctx.setLineDash([1, 2]);
         this.ctx.strokeStyle = '#E3E3E3';
@@ -99,6 +93,25 @@ class LineChart {
     this.ctx.restore();
   }
 
+  #drawLineChart = () => {
+    const { startTime, chartWidth, chartHeight } = this;
+
+    this.ctx.beginPath();
+    this.data.forEach((datum, index) => {
+      const { time, value } = datum;
+
+      const xPoint = ((time - startTime) / DURATION) * chartWidth * 0.2;
+      const yPoint = chartHeight - (value / MAX_Y) * this.chartHeight * 0.9;
+
+      if (!index) {
+        this.ctx.moveTo(xPoint, yPoint);
+      } else {
+        this.ctx.lineTo(xPoint, yPoint);
+      }
+    });
+    this.ctx.stroke();
+  };
+
   #draw = () => {
     this.#setTime();
     this.ctx.clearRect(
@@ -108,16 +121,25 @@ class LineChart {
       this.chartHeight + DEFAULT_AXIS_PADDING
     );
 
+    this.#drawLabelX();
     this.#drawLabelY();
 
     this.#drawAxisX();
     this.#drawAxisY();
 
-    this.#drawLabelX();
+    this.#drawLineChart();
 
     window.setInterval(() => {
       window.requestAnimationFrame(this.#draw);
     }, 5000);
+  };
+
+  getInitialData = (datum: Datum) => {
+    this.data.push(datum);
+  };
+
+  getUpdateData = (datum: Datum) => {
+    this.data.push(datum);
   };
 }
 
--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,9 @@ const $lineChart = document.querySelector('#lineChart') as HTMLCanvasElement;
 
 document.addEventListener('DOMContentLoaded', () => {
   const lineChart = new LineChart($lineChart);
+  lineChart.getInitialData({ time: Date.now(), value: Math.random() * 100 });
+
+  window.setInterval(() => {
+    lineChart.getUpdateData({ time: Date.now(), value: Math.random() * 100 });
+  }, 5000);
 });
