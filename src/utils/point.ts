2728d6864a4dd036d0754ba3326fd9c82cf9b13d
areumsheep <areumsheep@gmail.com>
2023-03-09 11:36:26 +0900

    Update: 비슷한 연산 반복하지 않도록 memo 변수 추가

--- src/utils/point.ts
+++ src/utils/point.ts
@@ -1,5 +1,10 @@
 import type { Point } from '../types/LineChart';
 
+type DistanceMemoType = {
+  [key: string]: number;
+};
+const memoPointsDistance: DistanceMemoType = {};
+
 const getDistance = ([x1, y1, x2, y2]: [number, number, number, number]) => {
   const dx = x2 - x1;
   const dy = y2 - y1;
@@ -15,7 +20,16 @@ export const findNearestPoint = (
   let nearestPoint;
   for (let i = 0; i < points.length; i++) {
     const { x, y } = points[i];
-    const distance = getDistance([x, y, mouseX, mouseY]);
+    const key = `${x},${y}`;
+    let distance;
+
+    if (memoPointsDistance[key]) {
+      distance = memoPointsDistance[key];
+    } else {
+      distance = getDistance([x, y, mouseX, mouseY]);
+      memoPointsDistance[key] = distance;
+    }
+
     if (distance < minDistance) {
       minDistance = distance;
       nearestPoint = [x, y];
